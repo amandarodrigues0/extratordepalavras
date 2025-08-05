0aa420a296d92c182344e13312ca47db8c3bb757
amandarodrigues0 <amanda.rodrigues.paulino@escola.pr.gov.br>
2025-08-05 12:37:28 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 function mostraPalavrasChave  (){
   const texto = document.querySelector('#entrada-de-texto').value;
   const campoResultado = document.querySelector('#resultado-palavrachave');
+const palavrasChave = processaTexto(texto);
 
-  campoResultado.textContent = texto;
+
+  campoResultado.textContent = palavrasChave.join(", ");
+}
+
+function processaTexto(texto){
+  let palavras = texto.split(/\P{L}+/u);
+
+let frequencias
+
+  return palavras;
 }
