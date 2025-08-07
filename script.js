ee76be3d246fae91474dfd43b94add6769b29de6
amandarodrigues0 <amanda.rodrigues.paulino@escola.pr.gov.br>
2025-08-07 12:34:31 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -14,7 +14,17 @@ const palavrasChave = processaTexto(texto);
 function processaTexto(texto){
   let palavras = texto.split(/\P{L}+/u);
 
-let frequencias
+let frequencias = {};
+for(let i of palavras){
+  frequencias[i] = 0;
+  for (let j of palavras){
+    if (i == j){
+      frequencias[i]++;
+       }
+  }
+}
+
+console.log(frequencias);
 
   return palavras;
 }
